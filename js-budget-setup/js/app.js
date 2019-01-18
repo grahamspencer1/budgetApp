e3a43b1c1149c320c55fedd1f71867c9d53101b9
Graham Spencer <grahamspence@gmail.com>
2019-01-18 16:09:57 -0500

    added error message when calculating an empty field or 0

--- js-budget-setup/js/app.js
+++ js-budget-setup/js/app.js
@@ -15,9 +15,22 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+  // Submit budget method
+  submitBudgetForm() {
+    const value = this.budgetInput.value;
+    if(value==='' || value < 0){
+      this.budgetFeedback.classList.add('showItem');
+      this.budgetFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;
+      const self = this;
+      console.log(this);
+      setTimeout(function(){
+        self.budgetFeedback.classList.remove('showItem');
+      }, 4000)
+    }
+  }
 }
 
-function evenListeners() {
+function eventListeners() {
   const budgetForm = document.getElementById('budget-form');
   const expenseForm = document.getElementById('expense-form');
   const expenseList = document.getElementById('expense-list');
@@ -27,12 +40,13 @@ function evenListeners() {
 
   // Budget form submit
   budgetForm.addEventListener('submit', function(event) {
-
+    event.preventDefault();
+    ui.submitBudgetForm();
   })
 
   // Expense form submit
   expenseForm.addEventListener('submit', function(event) {
-
+    event.preventDefault();
   })
 
   // Expense list click
